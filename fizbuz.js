678ae1e08fde834db195c0c4aa7c4db35652a2d8
phil fogas <fogas@sfr.fr>
2022-06-08 23:43:17 +0200

    ajout css
    ajout css et d'autre sql

--- fizbuz.js
+++ fizbuz.js
@@ -1,60 +1,74 @@
 
 const liste = [1, 2, 3, 5, 15, 20, 30, 45, 10, 25, 90, 9, 7, 14, 22];
-
-
 //fizzbuz en une function round
-function fibuz(liste)
-{
-  for (let a in liste ){
+function fibuzd(liste) {
+  liste.forEach(a => {
     if (a / 3 == Math.round(a / 3) && a / 5 == Math.round(a / 5)) {
-      console.log ('Fizzbuzz');
+      document.write('Fizzbuzz<br>');
     } else if (a / 3 == Math.round(a / 3)) {
-      console.log ('Fizz');
+      document.write('Fizz<br>');
     } else if (a / 5 == Math.round(a / 5)) {
-      console.log ('Buz');
+      document.write('Buz<br>');
     } else {
-      console.log (a +'');
+      document.write(a + '<br>');
     }
-  };
-}
-console.log ('version fizzbuz round ');
-fibuz(liste);
 
+  })
+};
+document.write('<br>version fizzbuz modulo <br>');
+fibuzd(liste);
+
+
+//fizzbuz en une function modulo
+function fibuzm(liste) {
+  liste.forEach(a => {
+    if (a % 3 == 0 && a % 5 ==0) {
+      document.write('Fizzbuzz<br>');
+    } else if (a % 3 == 0) {
+      document.write('Fizz<br>');
+    } else if (a % 5 == 0) {
+      document.write('Buz<br>');
+    } else {
+      document.write(a + '<br>');
+    }
+
+  })
+};
+document.write('<br>version fizzbuz modulo <br>');
+fibuzm(liste);
 
 //fizzbuz en une function round  ES5
-function fibuz5(liste)
-{
-  liste.forEach(function(a) {
+function fibuz5(liste) {
+  liste.forEach(function (a) {
     if (a / 3 == Math.round(a / 3) && a / 5 == Math.round(a / 5)) {
-      console.log ('Fizzbuzz');
+      document.write('Fizzbuzz<br>');
     } else if (a / 3 == Math.round(a / 3)) {
-      console.log ('Fizz');
+      document.write('Fizz<br>');
     } else if (a / 5 == Math.round(a / 5)) {
-      console.log ('Buz');
+      document.write('Buz<br>');
     } else {
-      console.log (a +'');
+      document.write(a + '<br>');
     }
   });
 }
-console.log ('version fizzbuf5');
+document.write('<br>version fizzbuf5<br>');
 fibuz5(liste);
 
 
 //fizzbuz en une function round  ES6
-function fibu6(liste)
-{
-  for (const [index,a] of liste.entries() ) {
+function fibu6(liste) {
+  for (const [index, a] of liste.entries()) {
     if (a / 3 == Math.round(a / 3) && a / 5 == Math.round(a / 5)) {
-      console.log ('Fizzbuzz');
+      document.write('Fizzbuzz<br>');
     } else if (a / 3 == Math.round(a / 3)) {
-      console.log ('Fizz');
+      document.write('Fizz<br>');
     } else if (a / 5 == Math.round(a / 5)) {
-      console.log ('Buz');
+      document.write('Buz<br>');
     } else {
-      console.log (a +'');
+      document.write(a + '<br>');
     }
   };
 }
-console.log ('version fizzbuf6');
+document.write('<br>version fizzbuf6<br>');
 fibu6(liste);
 
--- sql.php
+++ sql.php
@@ -1,3 +1,24 @@
 <?php
 
-$sql = "SELECT CASE WHEN `number`/3=ROUND(`number`/3) AND `number`/5=ROUND(`number`/5) THEN 'FizBuzz' WHEN `number`/3= ROUND(`number`/3) THEN 'Fiz' WHEN `number`/5=ROUND(`number`/5) THEN 'Buzz' ELSE `number` END AS 'fizbuz' FROM `fix`;";
+$sqlround = "SELECT CASE 
+WHEN `number`/3=ROUND(`number`/3) AND `number`/5=ROUND(`number`/5) THEN 'FizBuzz' 
+WHEN `number`/3= ROUND(`number`/3) THEN 'Fiz' 
+WHEN `number`/5=ROUND(`number`/5) THEN 'Buzz' 
+ELSE `number` END AS 'fizbuz' 
+FROM `fix`;";
+
+
+
+$sqlmo = "SELECT CASE 
+WHEN MOD(`number`, 5) = 0 AND mod(`number`, 3) = 0 THEN 'Fizz Buzz' 
+WHEN MOD(`number`,  5) = 0 THEN 'Buzz' 
+WHEN MOD(`number`,  3) = 0 THEN 'Fizz' 
+ELSE `number` END AS fiz 
+FROM fix;";
+
+$sqlmp = "SELECT CASE 
+WHEN `number` % 5 = 0 AND `number` % 3 = 0 THEN 'fizzbuzz'
+WHEN `number` % 5 = 0 THEN 'buzz'
+WHEN `number` % 3 = 0 THEN 'fizz'
+ELSE `number` END AS fiz
+FROM fix;";
